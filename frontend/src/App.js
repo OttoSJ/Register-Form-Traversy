501c652a1aacdf0ee35e53aa2d3648d87fbcc88b
Otto Jones <ottosjonesjr@gmail.com>
2022-03-07 10:28:30 -0500

    added more functionality to registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
+
 function App() {
   return (
     <>
@@ -17,6 +20,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
